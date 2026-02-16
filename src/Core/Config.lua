311d3ba98f2fa5e5d8842e22261a400eeecf772f
Chris Turner <peavers@gmail.com>
2026-02-16 11:32:29 -0600

    chore: Disable until TWW Season starts, or Archon updates its builds with version for TWW

--- src/Core/Config.lua
+++ src/Core/Config.lua
@@ -2,6 +2,8 @@ local _, addon = ...
 
 addon.Config = {
 	DEBUG_ENABLED = false,
+	MAINTENANCE_MODE = true,  -- Set to false to show talents again
+	MAINTENANCE_MESSAGE = "Archon has not updated with new talent strings.\n\nThis will be back up and working with the start of the next Season in TWW.",
 
 	DIALOG = {
 		WIDTH = 600,
